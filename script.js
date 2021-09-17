1e753cf8b4466f8060ee1a84218f040d5d10a251
ccornybig2 <ccornybig2@seznam.cz>
2021-09-17 08:44:59 +0200

    final version for review

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
+/*  JS for interest calculator. Difference from examples  - we are using separate function validateAmount for validating the Amount field.
+    This provides better user experience as we can validate it every time it changes. */
+
+// here we get values from DOM and compute the final result
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //first validate the amount input, stop processing if false
+    if(validateAmount() == false) {
+        return false;
+    }
+
+    // then get values from DOM for computing
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
     
+    // get the year of the end
+    var year = new Date().getFullYear()+parseInt(years);
+    
+    // compute the interest
+    var interest = principal * years * rate /100;
+    
+    // show user the result
+    document.getElementById("result").innerHTML="If you deposit <strong>"+principal+"</strong>,\<br\>at an interest rate of <strong>"+rate+"</strong>%\<br\>You will receive an amount of <strong>"+interest+"</strong>,\<br\>in the year <strong>"+year+"</strong>.\<br\>"; 
 }
-        
+
+// updating the value of "range" input for better user experience
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+// lets validate the Amount input as it has to be positive number... moved from compute() function for better user experience
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+
+    if (typeof principal == "undefined" || principal <=0)
+    {
+        alert("Please enter a positive number.");
+        document.getElementById("principal").focus();
+        return false;
+    }
+}
